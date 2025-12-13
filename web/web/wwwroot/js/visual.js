10d9f93e04593e7a6eae0eede6e8e6fd86c20557
mysticdicc <richardfosterwork@gmail.com>
2025-12-13 16:40:55 +0000

    added small animation on click subnet card

---
+++ web/web/wwwroot/js/visual.js
@@ -0,0 +1,6 @@
+﻿function shakeonclick(id) {
+    element = document.getElementById(id);
+    element.style.animation = "shake .3s";
+
+    element.addEventListener("animationend", (e) => { element.style.animation = ""; })
+}
